a2b2b44df33f229f19b04b26ffa5b3908d602cb5
Neha Nayak <neehanayak.918@gmail.com>
2025-05-20 15:52:20 -0400

    Update remove-nth-node-from-end-of-list.py

--- DSA/Solutions/remove-nth-node-from-end-of-list.py
+++ DSA/Solutions/remove-nth-node-from-end-of-list.py
@@ -23,3 +23,30 @@ class Solution(object):
             curr = curr.next
         curr.next = curr.next.next
         return head
+
+################### USING TWO POINTER ##################
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        left = head
+        right = head
+        i = 0
+        while i < n:
+            right = right.next
+            i+=1
+        if right == None:
+            return head.next
+        while right.next != None:
+            right = right.next
+            left = left.next
+        left.next = left.next.next
+        return head
